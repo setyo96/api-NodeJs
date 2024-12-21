1ba6e1beba80c7a4fdd8c1e9eb9703f275626967
Setyo nugroho <setyo.nugroho@salt.co.id>
2024-12-21 10:17:22 +0700

    update token

--- src/index.js
+++ src/index.js
@@ -136,7 +136,7 @@ app.get('/api/content', (req, res) => {
 
 app.post('/api/send-wa-dg',bodyParser.json(), (req, res) => {
     const { val_message, val_phoneNumber } = req.body;
-    const token = 'zjdYxrNX9r+vd_@Hy8rj'; 
+    const token = 'yP6E5SFThY1w77eGR5S6'; 
     // const phoneNumber = '6287778360195-1545882126@g.us';
     // const phoneNumber = '6285951391878'
     const countryCode = '62';
